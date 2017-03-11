86a7b9185be6f033b71b39e1adc5364a2c5fec1e
qizl <q@qizl.cn>
2017-03-11 17:55:36 +0800

    -

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        private int _i = 10;
+
+        public void SayHello()
+        {
+            this._i = 0;
+            var j = 123 / this._i;
+
+            Console.Write("hello!");
+        }
+
+        public void SayHelloAgain()
+        {
+            Console.Write("hello!");
+        }
+    }
+}
---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static Class1 c = null;
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            c = new Class1();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            c.SayHello();
+        }
+
+        [TestMethod]
+        public void TestSayHello()
+        {
+            c.SayHello();
+        }
+
+        [TestMethod]
+        public void TestSayHelloAgain()
+        {
+            c.SayHelloAgain();
+        }
+    }
+}
